96920bf326674c337117cf357309c8a2775f0576
Guilherme Figueiredo <guilherme_figueiredo@outlook.com.br>
2022-07-08 16:19:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const primeiro = {
+    comedy: [
+        {
+            'title': 'Friends',
+            'director': 'Cameron Crowe',
+            'year': 19994
+        }
+    ]
+}
+
+const segundo = {
+    action: [
+        {
+            'title': 'Justice League',
+            'director': 'Zack Snyder',
+            'year': 2017
+        }
+    ]
+}
+
+const terceiro = {
+    fantasy: [
+        {
+            'title': 'Spirited Away ',
+            'director': 'Hayao Miyazaki',
+            'year': 2003
+        }
+    ]
+}
+
+if(primeiro.comedy[0].title === 'Friends'){
+    console.log("MY FAVORIT SERIES IS...");
+}else{
+    console.log("NOT EVEN 5 STARS...");
+}
